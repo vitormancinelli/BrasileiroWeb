027be0e93131c38ca76df52248a3122e86354284
vitor <vitor@DESKTOP-759UAEO>
2020-06-17 18:54:43 -0300

    juntando o pacote controller

--- BrasileiraoWeb/src/main/java/brasileiraoWeb/controller/ServingWebContentApplication.java
+++ BrasileiraoWeb/src/main/java/brasileiraoWeb/controller/ServingWebContentApplication.java
@@ -1,4 +1,4 @@
-package brasileraoWeb.application;
+package brasileiraoWeb.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
