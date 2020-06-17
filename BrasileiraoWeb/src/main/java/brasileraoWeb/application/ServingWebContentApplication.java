d99eb4c0ca38934ff9b09b00d5f2ecefaf475fcf
vitor <vitor@DESKTOP-759UAEO>
2020-06-17 18:52:11 -0300

    separando o pacote controller

--- BrasileiraoWeb/src/main/java/brasileraoWeb/application/ServingWebContentApplication.java
+++ BrasileiraoWeb/src/main/java/brasileraoWeb/application/ServingWebContentApplication.java
@@ -1,4 +1,4 @@
-package brasileiraoWeb.controller;
+package brasileraoWeb.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
